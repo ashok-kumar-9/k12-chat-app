d9cd7b21600f8d835639f10fd89f86ce6e206e98
ashok-kumar-k12 <ashok.kr@orchids.edu.in>
2023-06-16 18:46:37 +0530

    changes in login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -22,151 +22,153 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _saving = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ModalProgressHUD(
-        inAsyncCall: _saving,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: SizedBox(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 48.0,
-              ),
-              TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: kTextFieldDecoration,
-              ),
-              const SizedBox(
-                height: 8.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.text,
-                onChanged: (value) {
-                  //Do something with the user input.
-                  password = value;
-                },
-                style: kTextInputStyle,
-                decoration: kTextFieldDecoration.copyWith(
-                    hintText: 'Enter your password'),
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Material(
-                  color: Colors.lightBlueAccent,
-                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                  elevation: 5.0,
-                  child: MaterialButton(
-                    onPressed: () async {
-                      //Implement login functionality.
-                      setState(() {
-                        _saving = true;
-                      });
-                      try {
-                        var user = await _auth.signInWithEmailAndPassword(
-                            email: email!, password: password!);
-                        Fluttertoast.showToast(
-                          msg: 'Welcome to Flash Chat',
-                          toastLength: Toast.LENGTH_LONG,
-                          gravity: ToastGravity.CENTER,
-                          timeInSecForIosWeb: 2,
-                          backgroundColor: Colors.white,
-                          textColor: Colors.blue[800],
-                          fontSize: 16.0,
-                        );
-                        Navigator.pushNamed(context, ChatScreen.id);
-                        setState(() {
-                          _saving = false;
-                        });
-                      } on FirebaseAuthException catch (e) {
-                        setState(() {
-                          _saving = false;
-                        });
-                        String errorMessage;
-                        switch (e.code) {
-                          case "invalid-email":
-                            errorMessage = "Enter a valid email address";
-                            break;
-                          case "wrong-password":
-                            errorMessage = "Your password is wrong.";
-                            break;
-                          case "user-not-found":
-                            errorMessage = "Email isn't registered";
-                            break;
-                          case "user-disabled":
-                            errorMessage =
-                                "User with this email has been disabled.";
-                            break;
-                          case "too-many-requests":
-                            errorMessage =
-                                "Too many requests. Try again later.";
-                            break;
-                          case "operaetion-not-allowed":
-                            errorMessage =
-                                "Signing in with Email and Password is not enabled.";
-                            break;
-                          default:
-                            errorMessage = "An undefined Error happened.";
-                        }
-                        //print(e.code);
-                        Fluttertoast.showToast(
-                          msg: errorMessage,
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 2,
-                          backgroundColor: Colors.red[400],
-                          textColor: Colors.white,
-                          fontSize: 16.0,
-                        );
-                      }
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: const Text(
-                      'Log In',
+    return SafeArea(
+      child: Scaffold(
+        body: ModalProgressHUD(
+          inAsyncCall: _saving,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: SizedBox(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
                     ),
                   ),
                 ),
-              ),
-              TextButton(
-                child: const Text(
-                  'Sign Up Instead',
+                const SizedBox(
+                  height: 48.0,
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, RegistrationScreen.id);
-                },
-              ),
-              TextButton(
-                child: const Text(
-                  'Forgot Password',
+                TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: kTextFieldDecoration,
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, ResetPasswordScreen.id);
-                },
-              ),
-            ],
+                const SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.text,
+                  onChanged: (value) {
+                    //Do something with the user input.
+                    password = value;
+                  },
+                  style: kTextInputStyle,
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password'),
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.lightBlueAccent,
+                    borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                    elevation: 5.0,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        //Implement login functionality.
+                        setState(() {
+                          _saving = true;
+                        });
+                        try {
+                          var user = await _auth.signInWithEmailAndPassword(
+                              email: email!, password: password!);
+                          Fluttertoast.showToast(
+                            msg: 'Welcome to Flash Chat',
+                            toastLength: Toast.LENGTH_LONG,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 2,
+                            backgroundColor: Colors.white,
+                            textColor: Colors.blue[800],
+                            fontSize: 16.0,
+                          );
+                          Navigator.pushNamed(context, ChatScreen.id);
+                          setState(() {
+                            _saving = false;
+                          });
+                        } on FirebaseAuthException catch (e) {
+                          setState(() {
+                            _saving = false;
+                          });
+                          String errorMessage;
+                          switch (e.code) {
+                            case "invalid-email":
+                              errorMessage = "Enter a valid email address";
+                              break;
+                            case "wrong-password":
+                              errorMessage = "Your password is wrong.";
+                              break;
+                            case "user-not-found":
+                              errorMessage = "Email isn't registered";
+                              break;
+                            case "user-disabled":
+                              errorMessage =
+                                  "User with this email has been disabled.";
+                              break;
+                            case "too-many-requests":
+                              errorMessage =
+                                  "Too many requests. Try again later.";
+                              break;
+                            case "operaetion-not-allowed":
+                              errorMessage =
+                                  "Signing in with Email and Password is not enabled.";
+                              break;
+                            default:
+                              errorMessage = "An undefined Error happened.";
+                          }
+                          //print(e.code);
+                          Fluttertoast.showToast(
+                            msg: errorMessage,
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 2,
+                            backgroundColor: Colors.red[400],
+                            textColor: Colors.white,
+                            fontSize: 16.0,
+                          );
+                        }
+                      },
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: const Text(
+                        'Log In',
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  child: const Text(
+                    'Sign Up Instead',
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                ),
+                TextButton(
+                  child: const Text(
+                    'Forgot Password',
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, ResetPasswordScreen.id);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
